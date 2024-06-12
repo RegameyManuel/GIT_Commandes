5d86efd768dbedd65067b051168b6d8eea525b31
manwax <102606433+RegameyManuel@users.noreply.github.com>
2024-06-12 23:58:48 +0200

    Add files via upload

--- texte.py
+++ texte.py
@@ -18,18 +18,17 @@ print(texte + separation)
 
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
-import lorem
 
 def generate_sentence():
-    sentence = lorem.sentence()
+    sentence = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     display_text(sentence + separation)
 
 def generate_paragraph():
-    paragraph = lorem.paragraph()
+    paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis porttitor congue. Vestibulum mauris nibh, convallis et risus sit amet, eleifend accumsan tellus. Sed quis feugiat lorem, non porta diam. Mauris vitae felis at risus tincidunt tempus. Morbi aliquet felis magna, et dignissim turpis porta et. Donec fringilla odio euismod diam viverra, a placerat arcu eleifend. Nulla a lorem pellentesque, consectetur dolor id, semper massa."
     display_text(paragraph + separation)
 
 def generate_text():
-    text = lorem.text()
+    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis porttitor congue. Vestibulum mauris nibh, convallis et risus sit amet, eleifend accumsan tellus. Sed quis feugiat lorem, non porta diam. Mauris vitae felis at risus tincidunt tempus. Morbi aliquet felis magna, et dignissim turpis porta et. Donec fringilla odio euismod diam viverra, a placerat arcu eleifend. Nulla a lorem pellentesque, consectetur dolor id, semper massa. Pellentesque dapibus imperdiet dui ornare ultrices. Nullam eget feugiat erat, eu tincidunt libero. Nulla tempus dolor eu ultricies pharetra. Cras neque sem, lobortis sit amet nisl at, rhoncus facilisis ex. Nunc at mollis nibh. Ut vel magna ligula. Phasellus vel diam vulputate, posuere quam ut, molestie dui. Maecenas neque mauris, lacinia quis vulputate dignissim, ultricies a nulla. Pellentesque fringilla, velit ut vestibulum faucibus, quam lorem efficitur elit, sed hendrerit lorem purus fringilla lacus. Fusce pretium finibus molestie. In at pharetra elit. Mauris arcu libero, ornare vel ligula eget, congue laoreet justo. Sed finibus imperdiet vestibulum. Morbi in placerat tortor, aliquam vulputate neque. Donec ultricies justo vitae massa varius, ac facilisis diam sagittis. Nullam consequat elit ac nunc tincidunt, eget condimentum risus consequat. Suspendisse non pretium ex. In nisi arcu, pretium non volutpat sed, aliquam quis leo. Aliquam consequat blandit nulla nec rhoncus. Suspendisse risus neque, dictum ut consequat a, ultricies et sapien. Nam id congue magna. Curabitur lacinia vitae tellus id pretium. Nulla facilisi. Maecenas sed magna sem. Cras ac lacus metus. Integer lobortis eros sit amet nisl tristique pretium. Nunc tristique est vitae posuere elementum. Fusce tincidunt at diam non venenatis. Cras fermentum mi metus, ac fringilla ligula viverra mollis. Ut eget dictum velit. Morbi volutpat in est at luctus. Praesent vestibulum velit nec orci malesuada, ut auctor tortor cursus. Nunc tempor volutpat sem at elementum. Nam nec metus ultricies, rhoncus purus nec, tincidunt purus. Nullam ullamcorper molestie volutpat. Nam viverra libero id dui pellentesque, eu gravida felis laoreet. Nulla sed fermentum quam. Duis id lectus sem. Maecenas volutpat ligula eget hendrerit lacinia. Sed laoreet non est quis semper. "
     display_text(text + separation)
 
 def display_text(text):
